88cc910ad38664cdece5f6c86a5f43e0da70d8ad
nhat khoa <nhatkhoa.tin@gmail.com>
2021-04-18 19:58:54 +0700

    initial commit

---
+++ src/components/Balance.js
@@ -0,0 +1,40 @@
+import React, {useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+
+const Balance = () => {
+    const {incomeTransactions, expenseTransactions} = useContext(GlobalContext);
+
+    const incomeAmount = incomeTransactions.map(
+        incomeTransaction => incomeTransaction.incomeAmount
+    )
+    
+    const expenseAmount = expenseTransactions.map(
+        expenseTransaction => expenseTransaction.expenseAmount
+    )
+
+    const totalIncome = incomeAmount.reduce((acc, item) =>(acc+=item), 0).toFixed(2);
+
+    const totalExpense = expenseAmount.reduce((acc, item) =>(acc+=item), 0).toFixed(2)
+
+
+    return (
+        <div className="balance">
+            <h2>Your Balance</h2>
+            <h3>${(totalIncome-totalExpense).toFixed(2)}</h3>
+            <div className="income-expense">
+                <div className="plus">
+                    <h3>Income</h3>
+                    <p>+${totalIncome}</p>
+                </div>
+                <div className="minus">
+                    <h3>Expenses</h3>
+                    <p>-${totalExpense}</p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Balance
